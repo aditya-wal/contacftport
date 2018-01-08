5d8637c7922fea39470b6da4c48cba36c0987d1d
aditya-wal <aditya.y@westagilelabs.com>
2018-01-08 12:37:37 +0530

    Adds the following   * css for logo   * handles the failure of contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'contacts#index'
-  resources :contacts
+  resources :contacts do
+    get :failure, on: :collection
+  end
   get	'contacts/:provider/contact_callback' => 'contacts#index', as: :gmail_contacts
-  get 'contacts/failure' => 'contacts#failure'
 end
